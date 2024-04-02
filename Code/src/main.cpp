298a9edca4a7d1a6ba2179c75dbbdf171429ebe6
Adam Anderson <adaand00@gmail.com>
2024-04-02 22:33:14 +0200

    Pins

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,5 +1,26 @@
 #include <Arduino.h>
 
+#define SCL PIN_PB0
+#define SDA PIN_PB1
+
+#define LED_EN PIN_PB2
+
+#define RTC_CLK PIN_PB3
+
+#define TX PIN_PA1
+#define RX PIN_PA2
+
+#define LIGHT_IN PIN_PA3
+#define LIGHT_EN PIN_PA4
+
+#define PGOOD PIN_PA5
+
+#define BU1 PIN_PA6
+#define BU2 PIN_PA7
+
+#define LED_ADDR 0x34
+#define RTK_ADDR 0x52
+
 void setup() {
   // put your setup code here, to run once:
 }
